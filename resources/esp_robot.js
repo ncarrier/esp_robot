bd7b54154ceabf7b3b00b959a4b7f933fb96860c
Nicolas Carrier <carrier.nicolas0@gmail.com>
2019-11-30 22:37:43 +0100

    basic control of one wheel via the web client

--- resources/esp_robot.js
+++ resources/esp_robot.js
@@ -1,9 +1,33 @@
 var ws = new WebSocket("ws://esp_robot.local:81");
 
+var left_wheel_setpoint = 90;
+var right_wheel_setpoint = 90;
+var head_setpoint = 90;
+
 ws.onopen = function(e) {
-	ws.send("My name is John");
+	console.log("open");
 };
 ws.onerror = function(error) {
 	alert(`[error] ${error.message}`);
 };
-console.log(ws.readyState);
+
+send_message = function(e) {
+	// TODO put in range
+	var msg = {
+		left_wheel : left_wheel_setpoint,
+		right_wheel : right_wheel_setpoint,
+		head : head_setpoint,
+	};
+	ws.send(JSON.stringify(msg));
+}
+
+document.onkeypress = function(e) {
+	console.log('Frappe de la touche de code ' + e.which);
+	if (e.which == 101) {
+		right_wheel_setpoint = right_wheel_setpoint + 1;
+		send_message();
+	} else if (e.which == 106) {
+		right_wheel_setpoint = right_wheel_setpoint - 1;
+		send_message();
+	}
+}
